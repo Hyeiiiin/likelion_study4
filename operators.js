f89763486f5cfcbce1101b9d377c2f20a67b132c
Hyeiiiin <129861410+Hyeiiiin@users.noreply.github.com>
2023-05-10 15:24:35 +0900

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,53 @@
+/**
+ * 연산자
+ * - 1개의 표현식에 대해 산술, 대입, 
+ * 논리, 타입 등을 평가하여 값을 생성하는 작업
+ */
+
+// 1. 산술 연산자
+var a = 10;
+var b = 3;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+console.log(a);
+console.log(b);
+
+var c = 7;
+// console.log(++c);
+// console.log(c++);
+// console.log(--c);
+// console.log(c--);
+
+// 2. 대입 연산자, 할당 연산자
+var d = 2;
+d = 10;
+console.log(d);
+
+// console.log(d += 3); // d = d + 3
+// console.log(d -= 3);
+// console.log(d *= 3);
+// console.log(d /= 3);
+// console.log(d %= 3);
+
+// 3. 비교 연산자
+// console.log(2 == 2);
+// console.log(2 == "2"); //암묵적 형변환
+// console.log(2 === "2");
+
+// console.log(2 !== 2);
+// console.log(2 !== "2");
+// console.log(2 !=== "2");
+
+console.log(4 > 2);
+console.log(4 < 2);
+console.log(4 >= 2);
+console.log(4 <= 2);
+
+
+
+
