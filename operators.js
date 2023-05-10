a8d45760562c4fd762d130e099dcf16f19d3d6d0
Hyeiiiin <129861410+Hyeiiiin@users.noreply.github.com>
2023-05-10 15:38:29 +0900

    operators.js

--- operators.js
+++ operators.js
@@ -48,6 +48,38 @@ console.log(4 < 2);
 console.log(4 >= 2);
 console.log(4 <= 2);
 
+// 4. 논리 연산자
 
+// 논리합(or)
+// 피연산자 중에 하나라도 참이면 참
+// console.log(true || true);
+// console.log(true || false || false);
+// console.log(false || false);
+// console.log(2 > 3 || 5 == 5);
+
+
+// 논리곱(and)
+// 피연산자 모두가 참이어야 참
+// console.log(true && true);
+// console.log(true && false && false);
+// console.log(false && false);
+// console.log(2 > 3 && 5 == 5);
+// console.log(2 < 3 && 5 == 5);
+
+
+// 부정(not)
+// 피연산자의 boolean 값을 부정
+// console.log(!true);
+// console.log(!false);
+// console.log(!(5 > 4));
+
+
+// 5. typeof 연산자
+console.log(typeof 1);
+console.log(typeof "1");
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof Symbol());
+console.log(typeof null);
 
 
